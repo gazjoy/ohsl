1cf2dde808e48bcf69e7246fab53d55648e31c76
Gareth Joyce <gjoyce@iweb.co.uk>
2017-07-02 21:56:49 +0100

    Added inline logo.

--- pattern/branding/logo/base.php
+++ pattern/branding/logo/base.php
@@ -1,1 +1,4 @@
-<img src="<?php echo getUrl('build/img/logo/ohsl.svg'); ?>" class="u-img-responsive" width="260" height="260" />
+<!-- <img src="<?php echo getUrl('build/img/logo/ohsl.svg'); ?>" class="u-img-responsive" width="260" height="260" />
+ -->
+
+<img src="<?php echo getUrl('build/img/logo/ohsl-inline.svg'); ?>" class="u-img-responsive" width="260" height="260" />
