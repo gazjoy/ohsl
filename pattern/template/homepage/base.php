815ffb1d532d0bac9984af26fcfda6efd62023b9
Gareth Joyce <gjoyce@iweb.co.uk>
2017-07-03 21:53:38 +0100

    Progress, adding blocks to home.

--- pattern/template/homepage/base.php
+++ pattern/template/homepage/base.php
@@ -1,42 +1,92 @@
 <?php printPattern('component/header/base'); ?>
 
-<main role="main" class="u-container">
+<main role="main">
     
     <section class="u-clearfix">
-        <div class="u-section-xl">
-            <h1 class="hero-heading u-measure-md-9">We are committed to the prevention of ill health at work.</h1>
-            <div class="u-measure-md-7">
-                <p class="lead">We are a team of expert Occupational Hygienists, we can help your business by ensuring that workplaces are safe environments for your employees. We do this by identifying the hazards, evaluating the risks and recommending effective control measures.</p>
+        <div class="u-section-xl u-container">
+            <h1 class="hero-heading g-col-md-9 g-col-lg-9">We are <i class="u-text-brand-aqua">committed</i> to the prevention of ill health at work.</h1>
+            <div class="u-section-sm g-col-md-9 g-col-lg-7">
+                <p class="lead">We are a team of expert <a href="profile">Occupational Hygienists</a>, we can help your business by ensuring that workplaces are safe environments for your employees. We do this by identifying the hazards, evaluating the risks and recommending effective control measures.</p>
+                
+            </div>
+            <p><a href="/" class="btn btn--brand-steel">Book a Consultation</a></p>
+        </div>
+    </section>
+    
+    <section class="u-clearfix u-fill-neutral-white u-separator-top">
+        <div class="u-section-xl u-container">
+            <h2 class="hero-heading u-measure-md-9">Our Solutions</h2>
+
+            <ul class="u-list-unstyled grid g-gutter">
+            
+                <li class="g-col-sm-6 g-col-md-4 u-full-height">
+                    <a href="/" class="card">
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <div class="card__body">
+                            <h2>Noise Monitoring</h2>
+                            <p>We can conduct a workplace noise survey that will assist your company in complying with it’s duties.</p>
+                        </div>
+                    </a>
+                </li>
+                
+                <li class="g-col-sm-6 g-col-md-4 u-full-height">
+                    <a href="/" class="card">
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <div class="card__body">
+                            <h2>Air Monitoring</h2>
+                            <p>We offer a monitoring service for most substances deemed hazardous to health.</p>
+                        </div>
+                    </a>
+                </li>
+                
+                <li class="g-col-sm-6 g-col-md-4 u-full-height">
+                    <a href="/" class="card">
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <div class="card__body">
+                            <h2>Local Exhaust Ventilation (LEV)</h2>
+                            <p>We will provide a clear and concise report based on the Health and Safety Executive’s template.</p>
+                        </div>
+                    </a>
+                </li>
+                
+                <li class="g-col-sm-6 g-col-md-4 u-full-height">
+                    <a href="/" class="card">
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <div class="card__body">
+                            <h2>Hazardous Substances (COSHH)</h2>
+                            <p>We have many years of experience in the application of COSHH and can provide: COSHH assessments.</p>
+                        </div>
+                    </a>
+                </li>
+                
+                <li class="g-col-sm-6 g-col-md-4 u-full-height">
+                    <a href="/" class="card">
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <div class="card__body">
+                            <h2>Indoor Air Quality &amp; Lighting</h2>
+                            <p>We are able to assist employers with this duty by conducting indoor air quality and lighting surveys.</p>
+                        </div>
+                    </a>
+                </li>
+                
+                <li class="g-col-sm-6 g-col-md-4 u-full-height">
+                    <a href="/" class="card">
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <div class="card__body">
+                            <h2>Occupational Hygiene Training</h2>
+                            <p>We have many years experience of delivering training courses.</p>
+                        </div>
+                    </a>
+                </li>
+                
+            </ul>
+            
+            <div class="u-section-sm">
+                <p class="lead">🇬🇧🇪🇺We provide quality occupational hygiene solutions throughout the UK and Europe.</p>
             </div>
         </div>
     </section>
-            
-            <hr>
-            
-            <h2 class="h1 u-measure-md-6">🇬🇧<em>OHSL provide quality occupational hygiene solutions throughout the UK and Europe.</em>🇪🇺</h2>
-        <h2>Our Services</h2>
-        
-        <ul>
-            <li>
-                <h2>Noise Monitoring</h2>
-            </li>
-            <li>
-                <h2>Air Monitoring</h2>
-            </li>
-            <li>
-                <h2>Local Exhaust Ventilation (LEV)</h2>
-            </li>
-            <li>
-                <h2>Hazardous Substances (COSHH)</h2>
-            </li>
-            <li>
-                <h2>Indoor Air Quality & Lighting</h2>
-            </li>
-            <li>
-                <h2>Occupational Hygiene Training</h2>
-            </li>
-        </ul>
-
+    
      <!--  
 <h1>Occupational Hygiene Solutions Ltd. is a dynamic, professional consultancy service provider offering bespoke, quality occupational hygiene solutions to a wide range of industry sectors throughout the UK and Europe.</h1>
 <p class="lead">Based in the Midlands the founders offer over 40 years of Occupational Hygiene experience across a diverse range of disciplines.</p>
