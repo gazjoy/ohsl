a1941f3c2abfd7f61109686e21fb836efc636323
Gareth Joyce <gjoyce@iweb.co.uk>
2017-08-07 21:17:14 +0100

    Services images updated.

--- pattern/template/homepage/base.php
+++ pattern/template/homepage/base.php
@@ -88,7 +88,7 @@
             
                 <div class="g-col-sm-6 g-col-md-4 u-full-height">
                     <a href="/" class="card">
-                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" alt="Noise Monitoring" />
                         <div class="card__body">
                             <h3 class="h4">Noise Monitoring</h3>
                             <p>We can conduct a workplace noise survey that will assist your company in complying with it’s duties.</p>
@@ -98,7 +98,7 @@
                 
                 <div class="g-col-sm-6 g-col-md-4 u-full-height">
                     <a href="/" class="card">
-                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <img src="<?php echo getUrl('build/img/services/air-monitoring.jpg'); ?>" width="590" height="325" alt="Air Monitoring" />
                         <div class="card__body">
                             <h3 class="h4">Air Monitoring</h3>
                             <p>We offer a monitoring service for most substances deemed hazardous to health.</p>
@@ -108,7 +108,7 @@
                 
                 <div class="g-col-sm-6 g-col-md-4 u-full-height">
                     <a href="/" class="card">
-                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <img src="<?php echo getUrl('build/img/services/local-exhaust-ventilation.jpg'); ?>" width="590" height="325" alt="Local Exhaust Ventilation (LEV)" />
                         <div class="card__body">
                             <h3 class="h4">Local Exhaust Ventilation (LEV)</h3>
                             <p>We will provide a clear and concise report based on the Health and Safety Executive’s template.</p>
@@ -118,7 +118,7 @@
                 
                 <div class="g-col-sm-6 g-col-md-4 u-full-height">
                     <a href="/" class="card">
-                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <img src="<?php echo getUrl('build/img/services/hazardous-substances.jpg'); ?>" width="590" height="325" alt="Hazardous Substances (COSHH)" />
                         <div class="card__body">
                             <h3 class="h4">Hazardous Substances (COSHH)</h3>
                             <p>We have many years of experience in the application of COSHH and can provide: COSHH assessments.</p>
@@ -128,7 +128,7 @@
                 
                 <div class="g-col-sm-6 g-col-md-4 u-full-height">
                     <a href="/" class="card">
-                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <img src="<?php echo getUrl('build/img/services/indoor-air-quality-lighting.jpg'); ?>" width="590" height="325" alt="Indoor Air Quality &amp; Lighting" />
                         <div class="card__body">
                             <h3 class="h4">Indoor Air Quality &amp; Lighting</h3>
                             <p>We are able to assist employers with this duty by conducting indoor air quality and lighting surveys.</p>
@@ -138,7 +138,7 @@
                 
                 <div class="g-col-sm-6 g-col-md-4 u-full-height">
                     <a href="/" class="card">
-                        <img src="<?php echo getUrl('build/img/services/noise-monitoring.jpg'); ?>" width="590" height="325" />
+                        <img src="<?php echo getUrl('build/img/services/occupational-hygiene-training.jpg'); ?>" width="590" height="325" alt="Occupational Hygiene Training" />
                         <div class="card__body">
                             <h3 class="h4">Occupational Hygiene Training</h3>
                             <p>We have many years experience of delivering training courses.</p>
