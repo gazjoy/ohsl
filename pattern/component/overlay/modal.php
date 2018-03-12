ff35fa9fb86ed5c174a03e968e74701c43fab589
Gareth.Joyce <gareth.joyce@GarethJoyces-MacBook-Pro.local>
2018-03-12 18:43:31 +0000

    Remove dave link.

--- pattern/component/overlay/modal.php
+++ pattern/component/overlay/modal.php
@@ -8,7 +8,7 @@
                 <div class="card__header">
                     <h2>Modal Content 1</h2>
                 </div>
-                <div class="u-block"> <a href="https://daveredfern.com">Hello</a>
+                <div class="u-block">
                     <?php printPattern('base/paragraph/base'); ?>
                     <p>Vestibulum varius sem vitae diam venenatis porttitor. <a href="http://google.com">Pellentesque</a> nec quam eget eros tincidunt mattis non ac nunc. Vivamus tristique, turpis vitae efficitur laoreet, eros tellus lobortis nulla, ac auctor neque velit at risus. Sed volutpat dolor erat, placerat malesuada quam tempor eu. Morbi a tellus sit amet lectus tincidunt imperdiet a tincidunt est. Cras vel volutpat mauris. Sed consectetur felis vel maximus elementum. Proin sed nisl lectus. Nunc non est ac ipsum gravida commodo vel quis nunc.</p>
                 </div>
@@ -23,7 +23,7 @@
                 <div class="card__header">
                     <h2>Modal Content 2</h2>
                 </div>
-                <div class="u-block"><a href="https://daveredfern.com">Hello</a>
+                <div class="u-block">
                     <?php printPattern('base/paragraph/base'); ?>
                 </div>
                 <div class="card__footer">
